b3f49531a532b1a64e3b87717a6aae62aaa8bc8c
Oguz <oguzsoydemir_07@hotmail.com>
2022-09-09 16:03:39 +0300

    Login page UI design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+/* Components */
 import Login from './Component/Login/Login';
-import './App.css';
+import Main from './Component/Main/Main'
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/mainpage" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 
+import "./Login.css"
+
 function Login() {
     return (
-      <div >
-        
-      </div>
+        <div className="login-container">
+            <div className="row">
+                <div className="col-md">
+                    <div className="card shadow">
+                        <div className="card-body">
+                            <h5 className="card-title">Sign in</h5>
+                            <hr />
+                            <div className="row mb-1">
+                                <div className="col-md">
+                                    <input id="username" className="form-control" placeholder="Username" type="text" />
+                                </div>
+                            </div>
+                            <div className="row mb-1">
+                                <div className="col">
+                                    <input id="password" className="form-control" placeholder="Password" type="password" />
+                                </div>
+                            </div>
+                            <hr />
+                            <div className="row float-center mb-1">
+                                <div className="col button-container">
+                                    <button id="btnLogin" className="btn btn-success">Login</button>
+                                    <button id="btnCancel" className="btn btn-danger">Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-  }
-  
-  export default Login;
+}
+
+export default Login;
